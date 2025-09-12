7d9c6e71964f274f43ae132352b4069f00c1a499
low33 <qnm835@gmail.com>
2025-09-12 18:38:45 +0900

    cors 설정을 위해 OPTIONS 메소드는 통과되도록 수정

--- src/main/java/org/example/workingmoney/config/security/SecurityConfig.java
+++ src/main/java/org/example/workingmoney/config/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -89,6 +90,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth ->
                         auth
+                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                                 .requestMatchers(allowedPath).permitAll()
                                 .anyRequest().authenticated()
                 )
